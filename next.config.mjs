af8ef868cebee1e9ebf63e7a24e6085e7b684e23
Piyush <piyushrane20@gmail.com>
2024-11-20 18:09:50 +0530

    first commit

---
+++ next.config.mjs
@@ -0,0 +1,15 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+
+    typescript: {
+        ignoreBuildErrors: true,
+      },
+      
+    images: {
+        domains: [
+            "utfs.io"
+        ]
+    }
+};
+
+export default nextConfig;
